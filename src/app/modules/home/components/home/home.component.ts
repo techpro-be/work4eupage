7ce5af2a6dbb0c8085c4498ba393f358b2703047
mario-neb <50882870+mario-neb@users.noreply.github.com>
2019-10-29 17:44:06 +0100

    Configured form

--- src/app/modules/home/components/home/home.component.ts
+++ src/app/modules/home/components/home/home.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { HomeService } from '../../services/home.service';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { HomeService } from '../../services/home.service';
 import { MatSnackBar } from '@angular/material';
-import { cvData } from 'src/app/shared/models/cvdata.model';
-import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,41 +10,21 @@ import { Observable } from 'rxjs';
 })
 export class HomeComponent implements OnInit {
 
+  // get firstName() { return this.cvForm.get('firstName'); }
+  // get lastName() { return this.cvForm.get('lastName'); }
+  // get age() { return this.cvForm.get('age'); }
+  // get phone() { return this.cvForm.get('phone'); }
+  // get email() { return this.cvForm.get('email'); }
+  // get address() { return this.cvForm.get('address'); }
+  // get message() { return this.cvForm.get('message'); }
 
-  get firstName() { return this.cvForm.get('firstName'); }
-  get lastName() { return this.cvForm.get('lastName'); }
-  get age() { return this.cvForm.get('age'); }
-  get phone() { return this.cvForm.get('phone'); }
-  get email() { return this.cvForm.get('email'); }
-  get address() { return this.cvForm.get('address'); }
-  get message() { return this.cvForm.get('message'); }
-
-  constructor(private afs: AngularFirestore) {
-
-    this.cvRef = this.afs.collection<cvData>('cvForm');
-    this.cvdata = {
-      firstName: this.firstName.value,
-      lastName: this.lastName.value,
-      age: this.age.value,
-      phone: this.phone.value,
-      email: this.email.value,
-      address: this.address.value,
-      message: this.message.value,
-    };
-}
+  constructor(
+    private homeService: HomeService,
+    private snackBar: MatSnackBar
+    ) {}
 
   cvForm: FormGroup;
   isLoading: boolean;
-  data: cvData[];
-
-
-
-
-  cvdata: cvData;
-  successMsg = 'Data successfully saved.';
-
-  cvRef: AngularFirestoreCollection<cvData>;
-  cvData: Observable<cvData[]>;
 
   ngOnInit() {
     this.cvForm = new FormGroup(
@@ -58,13 +36,13 @@ export class HomeComponent implements OnInit {
           validators: [Validators.required]
         }),
         age: new FormControl('', {
-          validators: [Validators.required]
+          validators: [Validators.required, Validators.minLength(18)]
         }),
         phone: new FormControl('', {
           validators: [Validators.required]
         }),
         email: new FormControl('', {
-          validators: [Validators.required]
+          validators: [Validators.required, Validators.email]
         }),
         address: new FormControl('', {
           validators: [Validators.required]
@@ -78,8 +56,30 @@ export class HomeComponent implements OnInit {
 
   onSubmit() {
     if (this.cvForm.valid) {
-      this.cvRef.add(this.cvdata).then( _ => alert(this.successMsg));
-    }
+      this.isLoading = true;
+      const data = this.cvForm.value;
+      this.homeService.createCv(data)
+      .then(
+        resp => {
+          this.snackBar.open('Your CV has been submitted successfully ', null, {
+            duration: 3000
+          });
+          this.isLoading = false;
+          this.cvForm.reset();
+          console.log('data', data);
+
+        })
+      .catch(error => {
+        this.isLoading = false;
+        this.snackBar.open(error.message, null, {
+          duration: 3000
+        });
+      });
+  } else {
+    this.snackBar.open('Please fill all the required fields', null, {
+      duration: 3000
+    });
+  }
   }
 }
 
--- src/app/modules/home/services/home.service.ts
+++ src/app/modules/home/services/home.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { cvData } from 'src/app/shared/models/cvdata.model';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,7 +9,15 @@ export class HomeService {
   constructor(private firestore: AngularFirestore) {}
 
 
-  createCv(data: cvData) {
-   return this.firestore.collection('cvForm').add(data);
-  }
+  createCv(data) {
+    return new Promise<any>((resolve, reject) => {
+        this.firestore
+            .collection('cvForm')
+            .add(data)
+            .then(
+              (res: any) => {
+              resolve(res);
+            }, err => reject(err));
+    });
+}
 }
