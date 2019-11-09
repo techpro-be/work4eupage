53e532302600ec550242643cc7e4e0144d671e1a
mario-neb <50882870+mario-neb@users.noreply.github.com>
2019-11-09 20:05:47 +0100

    deleted unused photos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDuW504W6IJ9__wExqGtSNWku497Xkr7XA',
+      authDomain: 'work4eupage.firebaseapp.com',
+      databaseURL: 'https://work4eupage.firebaseio.com',
+      projectId: 'work4eupage',
+      storageBucket: 'work4eupage.appspot.com',
+      messagingSenderId: '484275584478',
+      appId: '1:484275584478:web:6b2d333a95b21ee107f14f'
+    }
 };
